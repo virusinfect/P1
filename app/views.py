385aa05b97ec8d39517d34e5f37b2481e400488f
virusinfect <141482257+virusinfect@users.noreply.github.com>
2023-08-17 08:24:00 +0300

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def timeline(request):
+    return render(request, 'timeline.html')
+def test(request):
+    return render(request, 'test.html')
+def members(request):
+    return render(request, 'members.html')
