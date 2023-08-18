762453e3b4fff582f3aecec5e983aae5ebae826c
virusinfect <141482257+virusinfect@users.noreply.github.com>
2023-08-18 16:26:23 +0300

    created extra models
    added comments, main category, sub category, photos, tags

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from .models import Founder
+from .models import Founder, MainCategory, SubCategory, Photo, Comment, Tag
 
 admin.site.register(Founder)
+admin.site.register(MainCategory)
+admin.site.register(SubCategory)
+admin.site.register(Photo)
+admin.site.register(Comment)
+admin.site.register(Tag)
--- app/models.py
+++ app/models.py
@@ -7,3 +7,42 @@ class Founder(models.Model):
 
     def __str__(self):
         return self.name
+
+from django.db import models
+
+class MainCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    parent_category = models.ForeignKey(MainCategory, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag) 
+
+    def __str__(self):
+        return self.name
+
+class Photo(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='photos/')
+    category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    approved = models.BooleanField(default=True)
+    tags = models.ManyToManyField(Tag) 
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    text = models.TextField()
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
